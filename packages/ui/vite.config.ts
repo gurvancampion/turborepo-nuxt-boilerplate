0bf843cb81137bcbec502ebdce547693cb096898
Gurvan <gurvan.freelance@gmail.com>
2022-10-11 14:40:17 +0200

    refactor: remove vitest.config.ts and use only vite.config.ts

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,5 +1,13 @@
+/// <reference types="vitest" />
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 
-export default {
+export default defineConfig({
   plugins: [vue()],
-}
+  test: {
+    reporters: ['verbose'],
+    include: ['tests/**/*.test.ts'],
+    globals: true,
+    environment: 'jsdom',
+  },
+})
